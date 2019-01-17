d029e446b6c223c158b6b8cda7e3c27fb98f4c36
decand <deniejoey@gmail.com>
2019-01-17 16:32:07 +0700

    Fixing Image Preview Zoom pitch-out & Change Library

--- app/src/main/java/com/lmu/warungdananew/PreviewImageActivity.java
+++ app/src/main/java/com/lmu/warungdananew/PreviewImageActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.bogdwellers.pinchtozoom.ImageMatrixTouchHandler;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 
 public class PreviewImageActivity extends AppCompatActivity {
 
+//    ImageView imageView;
     ImageView imageView;
 
     boolean isImageFitToScreen;
@@ -29,13 +30,10 @@ public class PreviewImageActivity extends AppCompatActivity {
 
         Picasso.get()
                 .load(a.getStringExtra("photo"))
-                .memoryPolicy(MemoryPolicy.NO_CACHE)
-                .networkPolicy(NetworkPolicy.NO_CACHE)
                 .error(R.drawable.no_image)
                 .into(imageView);
 
-        PhotoViewAttacher photoViewAttacher = new PhotoViewAttacher(imageView);
-        photoViewAttacher.update();
+        imageView.setOnTouchListener(new ImageMatrixTouchHandler(getApplicationContext()));
 
 
     }
