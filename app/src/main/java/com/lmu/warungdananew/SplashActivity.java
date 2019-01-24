242413e3f63215385e3afeb8e4e6c3379dbf5fc7
decand <deniejoey@gmail.com>
2019-01-24 11:11:20 +0700

    Fix kirim laporan pooling ke semua aplikasi

--- app/src/main/java/com/lmu/warungdananew/SplashActivity.java
+++ app/src/main/java/com/lmu/warungdananew/SplashActivity.java
@@ -3,12 +3,15 @@ package com.lmu.warungdananew;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
